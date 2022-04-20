c1f8dd1b04fd7c0c5b6d46ea3421e1c0a06f4502
Joseph Merlino <joe_mer@hotmail.com>
2022-04-20 17:23:31 -0400

    Add project files.

---
+++ src/imgGllryIdServer.idp/Config.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace imgGllryIdServer.idp
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            {
+                new Client
+                {
+                     ClientName = "Image Gallery",
+                     ClientId = "imagegalleryclient",
+                     AllowedGrantTypes = GrantTypes.Code,
+                     RedirectUris = new string[] {"https://localhost:44389/signin-oidc"},
+                     PostLogoutRedirectUris = new string[]{ "https://localhost:44389/signin-oidc" },
+                     AllowedScopes = new string[]
+                     {
+                         IdentityServerConstants.StandardScopes.Profile,
+                         IdentityServerConstants.StandardScopes.OpenId
+                     },
+                     ClientSecrets = new Secret[]
+                     {
+                         new Secret("secret".Sha256())
+                     }
+                }
+            };
+    }
+}
---
+++ src/imgGllryIdServer.idp/Quickstart/TestUsers.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "818727",
+                        Username = "Claire",
+                        Password = "claire!!rox",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Claire Underwood"),
+                            new Claim(JwtClaimTypes.GivenName, "Claire"),
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                            new Claim(JwtClaimTypes.Email, "claireunderwood@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://claireU.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "88421113",
+                        Username = "Frank",
+                        Password = "frank!!rox",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Frank Underwood"),
+                            new Claim(JwtClaimTypes.GivenName, "Frank"),
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                            new Claim(JwtClaimTypes.Email, "frankunderwood@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://frankU.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
